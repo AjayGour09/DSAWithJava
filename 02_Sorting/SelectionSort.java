a865007e16869f95482a45766cca00045648ed65
Ajay Gour <ajaygour369@gmail.com>
2026-04-19 21:28:17 +0530

    second loop

--- 02_Sorting/SelectionSort.java
+++ 02_Sorting/SelectionSort.java
@@ -4,7 +4,7 @@ public class SelectionSort {
 
         for (int i = 0; i < n - 1; i++) {
             int minIndex = i;
-
+// second loop
             for (int j = i + 1; j < n; j++) {
                 if (arr[j] < arr[minIndex]) {
                     minIndex = j;
