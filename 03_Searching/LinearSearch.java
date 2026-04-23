71999e2a504a56ec6ecc8646d40a8e714e5f9980
Ajay Gour <ajaygour369@gmail.com>
2026-04-23 20:58:24 +0530

    Add comment to clarify loop in search method of LinearSearch class

--- 03_Searching/LinearSearch.java
+++ 03_Searching/LinearSearch.java
@@ -1,5 +1,6 @@
 public class LinearSearch {
     public static int search(int[] arr, int key) {
+        // loop
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == key)
                 return i;
