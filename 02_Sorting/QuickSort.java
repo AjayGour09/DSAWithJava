e95cabd7ea04f53876fb5507dc6208a7a36e4d70
Ajay Gour <ajaygour369@gmail.com>
2026-05-01 21:25:56 +0530

    pivot value

--- 02_Sorting/QuickSort.java
+++ 02_Sorting/QuickSort.java
@@ -11,7 +11,7 @@ public class QuickSort {
     }
 
     public static int partition(int[] arr, int low, int high) {
-        int pivot = arr[high]; // check pivot
+        int pivot = arr[high]; // check pivot value
         int i = low - 1;
 
         for (int j = low; j < high; j++) {
