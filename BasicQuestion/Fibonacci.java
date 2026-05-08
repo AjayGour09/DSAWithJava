551b8280d390ec5dfefebc4c9e21c04923ecb257
Ajay Gour <ajaygour369@gmail.com>
2026-05-08 20:42:36 +0530

    Fix typo in comment for Fibonacci method

--- BasicQuestion/Fibonacci.java
+++ BasicQuestion/Fibonacci.java
@@ -3,7 +3,7 @@ public class Fibonacci {
     public static int fib(int n) {
         if (n <= 1)
             return n;
-        return fib(n - 1) + fib(n - 2); // add first and second value
+        return fib(n - 1) + fib(n - 2); // add first and second values
     }
 
     public static void main(String[] args) {
